9e7afc18f363a791cf6d5374efd65ea8719065fa
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-04-18 15:56:29 +0500

    mysql venv are written

---
+++ python/src/server.py
@@ -0,0 +1,15 @@
+import jwt, datetime, os
+from flask import Flask, Request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+server.config["MY_SQL"] = os.environ.get("MY_SQL") 
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER") 
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD") 
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB") 
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT") 
+
+
+
+print(server.config["MY_SQL"])
